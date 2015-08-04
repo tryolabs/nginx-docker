103c73b19b2b74799d64f462b988adb779dabda9
Alan Descoins <dekked@gmail.com>
2015-08-04 16:10:12 -0300

    Add feedback on template evaluations.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # Copy all templates to /etc/nginx/, evaluate them and delete the files
 cp -R /templates/* /etc/nginx/
 for f in $(find /etc/nginx/ -type f -name "*.j2"); do
+    echo -e "Evaluating template\n\tSource: $f\n\tDest: ${f%.j2}"
     j2 $f > ${f%.j2}
     rm -f $f
 done
